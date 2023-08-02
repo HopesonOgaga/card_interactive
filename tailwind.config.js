8e4e4ab10617e2e0939ae85d7f2b3f2fa5601996
Hopeson <hopesonogaga@gmail.com>
2023-08-02 16:08:43 +0100

    card uui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('./images/bg-main-mobile.png')",
+      },
+      fontFamily: {
+        epilogue: "Epilogue, sans-serif",
+      },
+      colors: {
+        grad1: "hsl(249, 99%, 64%) ",
+        grad2: "hsl(278, 94%, 30%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
