820b0c6f5ef9dc784b7a0cefa55a652595dcea6c
Hopeson <hopesonogaga@gmail.com>
2023-08-08 14:38:48 +0100

    js info data

---
+++ build/scripts/main.js
@@ -0,0 +1,13 @@
+// dom
+const cvc_display = document.querySelector(".cvc_data");
+const display_number = document.querySelector(".number_display");
+const display_name = document.querySelector(".name_val");
+const display_date = document.querySelector(".date_value");
+// btn
+const enter_btn = document.querySelector(".btn_enter");
+//value
+const cardholder = document.querySelector(".cardholder_display");
+const card_number = document.querySelector(".card_number");
+const month = document.querySelector(".mm");
+const year = document.querySelector(".yy");
+const cvc_input = document.querySelector(".cvc_input");
